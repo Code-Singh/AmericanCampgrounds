ece21b584cc7934ccdf74888f4eca571b1e634aa
Dharamveer Singh <singh21206@gmail.com>
2023-02-22 23:42:56 -0500

    Add Success & Error Flash Messages

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Express Mongoose EJS Ejs-Mate Morgan
 Styled with Bootstrap 5
 
 Joi for Validation
+
+Express-Session and Connect-Flash for Session and Flash Messages
--- app.js
+++ app.js
@@ -6,13 +6,14 @@ const port = 3000;
 const methodOveride = require('method-override')
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError')
-
+const session = require('express-session')
+const flash = require('connect-flash');
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/american-campgrounds', 
     {useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useUnifiedTopology: true //mongoose deprecation errors
 });
 
 const db = mongoose.connection;
@@ -27,6 +28,28 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true})) //parse post request
 app.use(methodOveride('_method'))
+
+app.use(express.static(path.join(__dirname,'public')))
+
+const sessionConfig = {
+    secret: 'SECRET',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
 
