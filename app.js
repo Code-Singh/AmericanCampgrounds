03c23a1fb58c8dd45eb154cf4a2d4c02bda64c21
Dharamveer Singh <singh21206@gmail.com>
2023-02-22 21:41:10 -0500

    Break up routes with Express Router

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const app = express();
 const path = require('path')
 const mongoose = require('mongoose');
 const port = 3000;
-const Campground = require('./models/campground');
 const methodOveride = require('method-override')
-const { urlencoded } = require('express');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError')
-const catchAsync = require('./utils/catchAsync')
-const Joi = require('joi')
-const {campgroundSchema, reviewSchema}  = require('./schemas')
-const Review = require('./models/review')
 
+const campgrounds = require('./routes/campgrounds')
+const reviews = require('./routes/reviews')
 
 mongoose.connect('mongodb://localhost:27017/american-campgrounds', 
     {useNewUrlParser: true, 
@@ -31,95 +27,21 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({extended: true})) //parse post request
 app.use(methodOveride('_method'))
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
-
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('home')
-})
+});
 
-
-app.get('/campgrounds', catchAsync (async(req,res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-}))
-
-app.get('/campgrounds/new', (req,res) => { //place before :id to avoid / endpoint
-    res.render('campgrounds/new')
-})
-
-app.post('/campgrounds', validateCampground, catchAsync (async (req,res, next) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/:id', catchAsync (async(req,res) =>{
-    const campground = await Campground.findById(req.params.id).populate('reviews')
-    res.render('campgrounds/show', {campground})
-}))
-
-app.get('/campgrounds/:id/edit', catchAsync (async (req,res) =>{
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', {campground})
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync ( async(req,res) => {
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    //remove review refrence in Campground
-    await campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
-
-app.all('*', (req,res, next) =>{
+app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
 
 app.use((err, req, res, next) => {
-    const {statusCode = 500} = err;
-    if (!err.message) err.message = 'Something Went Wrong!';
-    res.status(statusCode).render('error', {err})
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 }) 
 
 app.listen(port, ()=> {
